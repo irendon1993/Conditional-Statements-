f5fcaf06b46556d83278de148a082794b066708e
irendon1993 <38363661+irendon1993@users.noreply.github.com>
2022-04-01 16:29:30 +0000

    Finished section

--- main.rb
+++ main.rb
@@ -1,1 +1,11 @@
-puts "Hello, World!"
+name = "Isaac"
+if name == "Isaac"
+  puts "Welcome to the program, Isaac"
+elsif name = "Jack"
+  puts "Welcome to the program, Jack"
+elsif name == "Evgeny"
+  puts "Go back to helping Students Evgeny"
+else 
+  puts "Welcome to the program, User"
+end
+
